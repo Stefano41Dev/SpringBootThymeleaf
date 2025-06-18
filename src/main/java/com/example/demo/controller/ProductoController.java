58bf17ef22772f3e458c87f811657d9619860c5e
Flash <flashasea@gmail.com>
2025-06-18 13:02:36 -0500

    se agrego la funcion de editar producto

--- src/main/java/com/example/demo/controller/ProductoController.java
+++ src/main/java/com/example/demo/controller/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,9 +41,25 @@ public class ProductoController {
 		return "redirect:/vistas/listarProductos";
 	}
 	@GetMapping("/editarProducto/{id}")
-	public String editarProducto(@RequestParam int id, Model modelo) {
+	public String editarProducto(@PathVariable int id, Model modelo) {
 		Producto productoBuscado = productoServicio.buscarProducto(id);
 		modelo.addAttribute("productoEditar",productoBuscado);
 		return "vistas/EditarProducto";
 	}
+	@PostMapping("/editarProducto")
+	public String editarProductoPost(@ModelAttribute Producto producto) {
+		Producto productoBuscado = productoServicio.buscarProducto(producto.getIdProducto());
+		if(productoBuscado != null) {
+			productoBuscado.setCantidad(producto.getCantidad());
+			productoBuscado.setCodigoBarras(producto.getCodigoBarras());
+			productoBuscado.setFechaVenta(producto.getFechaVenta());
+			productoBuscado.setNombreProducto(producto.getNombreProducto());
+			productoBuscado.setNroLote(producto.getNroLote());
+			productoBuscado.setPrecio(producto.getPrecio());
+			productoBuscado.setTotal(producto.getCantidad() * producto.getPrecio());
+			productoServicio.registrarProducto(productoBuscado);
+		}
+		return "redirect:/vistas/listarProductos";
+	}
+	
 }
--- src/main/java/com/example/demo/repository/IProductoRepositorio.java
+++ src/main/java/com/example/demo/repository/IProductoRepositorio.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.example.demo.model.Producto;
 
 public interface IProductoRepositorio extends CrudRepository<Producto, Integer> {
-
+	
 }
